dd22b1afdee636173eb614633b4a73737fc2c496
KD <39858155+khduyentr@users.noreply.github.com>
2022-08-29 09:00:25 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+1.Render song 
+2. Scroll
+3. Play - Pause
+4. CD rotation 
+5. Change song next|prev
+6. Random 
+7. Next / Repeat song
+8. Active song 
+9. Scroll active song to view
+10. Play song when clicked
+*/
+
+
